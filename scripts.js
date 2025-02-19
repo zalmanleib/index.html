2e6eb16d5372edfbf1b2ccca36e0f7de3fa3bd89
zalmanleib <154308683+zalmanleib@users.noreply.github.com>
2025-02-18 20:48:57 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@ document.addEventListener("DOMContentLoaded", function() {
                 <img src="https://lubavitchyeshiva.org/media/images/1310/QwiB13106920.png" class="logo" alt="School Logo">
             </a>
             <nav>
-                <a href="index.html">Home</a> |
-                <a href="about.html">About</a> |
-                <a href="contact.html">Contact</a> |
+                <a href="index.html">Home</a>
+                <a href="about.html">About</a>
+                <a href="contact.html">Contact</a>
                 <a href="login.html">Secure Login</a>
             </nav>
         </header>
