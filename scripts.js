7b36a9db8a7f29a247d30ea0b7692551a54c0c7d
zalmanleib <154308683+zalmanleib@users.noreply.github.com>
2025-02-18 20:44:34 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("header").innerHTML = `
         <header>
-            <h1>School Name</h1>
+            <a href="index.html">
+                <img src="https://lubavitchyeshiva.org/media/images/1310/QwiB13106920.png" class="logo" alt="School Logo">
+            </a>
             <nav>
                 <a href="index.html">Home</a> |
                 <a href="about.html">About</a> |
@@ -13,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     document.getElementById("footer").innerHTML = `
         <footer>
-            <p>&copy; 2025 School Name. All Rights Reserved.</p>
+            <p>&copy; 2025 Lubavitch Yeshiva. All Rights Reserved.</p>
         </footer>
     `;
 });
